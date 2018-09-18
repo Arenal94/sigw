f69bd19dad000fb44c0ada12879435babeb42af5
Alejandro Fernández Herrero <UO231537@uniovi.es>
2018-09-19 00:47:35 +0200

    commit con el mapa inicial

---
+++ rutas/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="all">
+ <GmapMap
+  :center="{lat:10, lng:10}"
+  :zoom="7"
+  map-type-id="terrain"
+  style="width: 100%; height: 100%"
+>
+</GmapMap>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+export default {
+  name: 'GoogleMap'
+}
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCJ5GI9TFx8kPwL8dyViS5UUDIybpH36IE',
+    libraries: 'places'
+  }
+})
+</script>
+<style>
+.all{height:100%}
+</style>
